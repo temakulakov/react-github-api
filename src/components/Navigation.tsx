ddfbf85c09c5c90ba3db26ab88843f612d159538
Artem Kulakov <109291017+temakulakov@users.noreply.github.com>
2023-02-11 08:01:11 +0300

    Add files via upload

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,12 +1,15 @@
-import React from 'react';
-import {Link} from "react-router-dom";
+import React from 'react'
+import {Link} from 'react-router-dom'
 
 export function Navigation() {
-    return <nav className={"flex justify-between items-center h-[50px] px-5 "}>
-        <h3 className={"mr-2 font-source-pro"}>GitHub search</h3>
-        <span>
-            <Link to={"/"} className={"mr-2 font-source-pro"}>Home</Link>
-            <Link to={"/favourites"} className={"font-source-pro"}>Favourites</Link>
-        </span>
+  return (
+    <nav className="flex justify-between items-center h-[50px] px-5 shadow-md bg-gray-500 text-white">
+      <h3 className="font-bold">Github Search</h3>
+
+      <span>
+        <Link to="/" className="mr-2">Home</Link>
+        <Link to="/favourites">Favourites</Link>
+      </span>
     </nav>
+  )
 }
